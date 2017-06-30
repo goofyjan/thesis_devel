175d3e522faf3228a813cb2462f508afc892ffcb
Jan Thröner <jan.throener@rub.de>
2017-06-30 15:12:08 +0200

    moveit tests. moveit_commander not working

--- mrk_robot_support/openGripper.sh
+++ mrk_robot_support/openGripper.sh
@@ -1,1 +1,1 @@
-rostopic pub /gripper_controller/command trajectory_msgs/JointTrajectory '{joint_names: ["r_finger_joint","l_finger_joint"], points: [{positions: [0.09,0.09], time_from_start: [1.0, 0.0]}]}' -1
+rostopic pub /gripper_controller/command trajectory_msgs/JointTrajectory '{joint_names: ["l_finger_joint","r_finger_joint"], points: [{positions: [0.09,0.09], time_from_start: [1.0, 0.0]}]}' -1
