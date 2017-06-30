546b9b913389cd8d97b630679930d116634149c7
Jan Thröner <jan.throener@rub.de>
2017-06-30 18:01:05 +0200

    changed world file. faster detach item

--- mrk_robot_support/closeGripper.sh
+++ mrk_robot_support/closeGripper.sh
@@ -1,1 +1,1 @@
-rostopic pub /gripper_controller/command trajectory_msgs/JointTrajectory '{joint_names: ["r_finger_joint","l_finger_joint"], points: [{positions: [0.055, 0.055], time_from_start: [1.0, 0.0]}]}' -1
+rostopic pub /gripper_controller/command trajectory_msgs/JointTrajectory '{joint_names: ["r_finger_joint","l_finger_joint"], points: [{positions: [0.05,0.05], time_from_start: [1.0, 0.0]}]}' -1
