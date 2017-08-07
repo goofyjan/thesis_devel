61a510a7be0dc2a8805d366576121d9045bc0dba
Jan Thröner <jan.throener@rub.de>
2017-08-07 10:52:50 +0200

    Python gripper open/close

---
+++ mrk_robot/scripts/close_gripper.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import rospy
+import copy
+import time
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+rospy.init_node('close_gripper')
+
+pub = rospy.Publisher('gripper_controller/command', JointTrajectory, queue_size=10)
+
+trajectory = JointTrajectory()
+points = JointTrajectoryPoint()
+
+trajectory.joint_names.append("r_finger_joint")
+trajectory.joint_names.append("l_finger_joint")
+points.positions = [0.06,0.06]
+points.time_from_start.secs = 1.0
+
+trajectory.points.append(points)
+rate = rospy.Rate(10);
+
+while not rospy.is_shutdown():
+     pub.publish(trajectory)
+     rate.sleep()
+
+rospy.spin()
